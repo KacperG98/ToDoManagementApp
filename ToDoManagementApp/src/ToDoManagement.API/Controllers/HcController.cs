17f8ca467d23aa89fb468555fe4ef821d0e53c97
KacperG98 <kandmgamestudio@gmail.com>
2021-11-04 13:28:04 +0100

    add hc controller skeleton

---
+++ ToDoManagementApp/src/ToDoManagement.API/Controllers/HcController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoManagement.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HcController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Healthy";
+        }
+    }
+}
